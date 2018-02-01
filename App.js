dd4f684ab83c5db02beeca918cdd62f8bcc59b5f
Fábio Maiorano <fsmaiorano@gmail.com>
2018-02-01 20:33:32 -0200

    add: first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/addEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+export default class App extends React.Component {
+
+  state = {
+    value: 0
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
+    );
+  }
+}
