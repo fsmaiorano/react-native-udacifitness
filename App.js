50934a131c893c166f16415ca3ba52861bfeb20f
Fábio Maiorano <fsmaiorano@gmail.com>
2018-02-05 21:02:58 -0200

    estável

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AddEntry from './components/addEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/history'
 
 export default class App extends React.Component {
 
@@ -14,8 +15,9 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={{flex:1}}>
-          <AddEntry />
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }}></View>
+          <History />
         </View>
       </Provider>
     );
