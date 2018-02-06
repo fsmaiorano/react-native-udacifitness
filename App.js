19702150605bacd2a6ed748b9a8266d3b4d874b0
Fábio Maiorano <fsmaiorano@gmail.com>
2018-02-06 21:25:15 -0200

    adiciona stack navigation

--- App.js
+++ App.js
@@ -5,9 +5,19 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/history'
-import { TabNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+import EntryDetail from './components/entryDetail'
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tabs = TabNavigator({
   History: {
@@ -41,18 +51,29 @@ const Tabs = TabNavigator({
     }
   })
 
-export default class App extends React.Component {
-
-  state = {
-    value: 0
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
   }
+})
+
+export default class App extends React.Component {
 
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <View style={{ height: 20 }}></View>
-          <Tabs />
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+          <MainNavigator />
         </View>
       </Provider>
     );
