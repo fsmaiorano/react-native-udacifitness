d75601345a1bd4f951fa86afd057f0131f7a72cf
Fábio Maiorano <fsmaiorano@gmail.com>
2018-02-06 20:47:48 -0200

    adiciona navigation

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/addEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/history'
+import { TabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    tabBarLabel: 'Add Entry',
+    tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+  }
+}, {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  })
 
 export default class App extends React.Component {
 
@@ -17,7 +52,7 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           <View style={{ height: 20 }}></View>
-          <History />
+          <Tabs />
         </View>
       </Provider>
     );
